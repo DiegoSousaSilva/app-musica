344cce7529310fdaaeddff1b20f1f48025a3ce12
Diego Sousa <derickbenji2@gmail.com>
2020-10-16 23:35:15 -0300

    ativando player de musicas

--- App.js
+++ App.js
@@ -13,44 +13,54 @@ export default function App() {
       nome: 'Kaen',
       artista: 'Dororo',
       playing: false,
-      file: '',
+      file: require('./src/midia/Dororo.mp3'),
     },
     {
-      nome: 'Ohayo Sekai, God Morning World',
-      artista: 'Dr. Stone',
+      nome: 'Black Cat',
+      artista: 'Black Clover',
       playing: false,
-      file: '',
-    },
-    {
-      nome: 'Inferno',
-      artista: 'Fire Force',
-      playing: false,
-      file: '',
+      file: require('./src/midia/BlackCat.mp3'),
     },
     {
       nome: 'Liar',
       artista: 'One Ok Rock',
       playing: false,
-      file: '',
+      file: require('./src/midia/Liar.m4a'),
     },
     {
       nome: 'Sasageyo',
       artista: 'Atack on Titan',
       playing: false,
-      file: '',
+      file: require('./src/midia/Sasageyo.mp3'),
     },
   ]);
 
-  const changeMusic = (id)=>{
+  const changeMusic = async (id) =>{
+    let curFile = null;
     let newMusic = musicas.filter((val, k)=>{
       if(id==k){
         musicas[k].playing = true;
+        curFile = musicas[k].file;
       }
       else {
         musicas[k].playing = false;
       }
       return musicas[k];
     })
+
+    if(audio != null){
+      audio.unloadAsync();
+    }
+
+    let curAudio = new Audio.Sound();
+
+    try{
+      await curAudio.loadAsync(curFile);
+      await curAudio.playAsync();
+      
+    }catch(error){}
+
+    setAudio(curAudio);
     setMusicas(newMusic);
   }
 
@@ -73,9 +83,9 @@ export default function App() {
           if(val.playing){
             return(
               <View style={styles.table}>
-                <TouchableOpacity style={styles.touchable} onPress={()=>{changeMusic(k)}}>
+                <TouchableOpacity style={styles.touchable} onPress={()=>changeMusic(k)}>
                   <Text style={[styles.touchableText, {color: '#1db954'}]}>
-                    <AntDesign name="play" size={15} color='#1db954' />  
+                    <AntDesign name="pause" size={15} color='#1db954' />  
                     {" "} {val.nome}
                   </Text>
                   <Text style={[styles.touchableText, {color: '#1db954'}]}>{val.artista}</Text>
@@ -86,7 +96,7 @@ export default function App() {
           }else{
             return(
               <View style={styles.table}>
-                <TouchableOpacity style={styles.touchable} onPress={()=>{changeMusic(k)}}>
+                <TouchableOpacity style={styles.touchable} onPress={()=>changeMusic(k)}>
                   <Text style={styles.touchableText}><AntDesign name="play" size={15} color='#fff'/>  {val.nome}</Text>
                   <Text style={styles.touchableText}>{val.artista}</Text>
                 </TouchableOpacity>
